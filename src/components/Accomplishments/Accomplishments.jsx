8c188dd153677774d83e52f75518d740957cfb44
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-15 14:36:51 -0500

    add all comp

---
+++ src/components/Accomplishments/Accomplishments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Accomplishments = () => {
+  return (
+    <div>Accomplishments</div>
+  )
+}
+
+export default Accomplishments
---
+++ src/components/BackgroundAnimation/BackgroundAnimation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BackgroundAnimation = () => {
+  return (
+    <div>BackgroundAnimation</div>
+  )
+}
+
+export default BackgroundAnimation
