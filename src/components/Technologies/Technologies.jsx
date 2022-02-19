aa354c6170afc97f52a795f342115fee069a7a76
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-19 18:14:52 -0500

    add tech comp

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () => {
   return (
-    <div>Technologies</div>
+   <Section id="tech">
+     <SectionDivider />
+     <SectionTitle>Technologies</SectionTitle>
+     <SectionText>
+       I have worked with a range .....
+     </SectionText>
+   </Section>
   )
 }
 
