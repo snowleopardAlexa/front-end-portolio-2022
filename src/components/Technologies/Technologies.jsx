278f34134fa1fca283e02c559063b7b6ed23e23c
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-19 18:24:08 -0500

    add technologies

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,4 +1,6 @@
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { BsPenFill } from 'react-icons/bs';
+import { DiReact } from 'react-icons/di';
+import { MdDesignServices } from 'react-icons/md';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
@@ -10,6 +12,48 @@ const Technologies = () => {
      <SectionText>
        I have worked with a range .....
      </SectionText>
+     <List>
+       <ListItem>
+         <DiReact size="3rem" />
+         <ListContainer>
+           <ListTitle>Front-End</ListTitle>
+           <ListParagraph>
+             Experience with <br />
+             React.js, Next.js, HTML, CSS, JavaScript
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+       <ListItem>
+         <MdDesignServices size="3rem" />
+         <ListContainer>
+           <ListTitle>UI/UX</ListTitle>
+           <ListParagraph>
+             Experience with <br />
+             Sketch, Figma
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+       <ListItem>
+         <BsPenFill size="3rem" />
+         <ListContainer>
+           <ListTitle>Technical Writing</ListTitle>
+           <ListParagraph>
+             An avid writer <br />
+             Technical Documentation
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+       <ListItem>
+         <BsPenFill size="3rem" />
+         <ListContainer>
+           <ListTitle>General Knowledge</ListTitle>
+           <ListParagraph>
+             Experience with <br />
+             Node, Express, MongoDB, Rest API, GraphQL
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+     </List>
    </Section>
   )
 }
