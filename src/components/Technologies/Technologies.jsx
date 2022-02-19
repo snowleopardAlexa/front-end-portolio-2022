89bb3a49456be9ac63726947c7e2b8f936c5dfa0
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-19 18:51:31 -0500

    add all technology info

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,6 +1,10 @@
 import { BsPenFill } from 'react-icons/bs';
 import { DiReact } from 'react-icons/di';
 import { MdDesignServices } from 'react-icons/md';
+import { GrGraphQl } from 'react-icons/gr';
+import { FaDatabase } from 'react-icons/fa';
+import { FaSketch } from 'react-icons/fa';
+import { CgWebsite } from 'react-icons/cg';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
@@ -10,46 +14,68 @@ const Technologies = () => {
      <SectionDivider />
      <SectionTitle>Technologies</SectionTitle>
      <SectionText>
-       I have worked with a range .....
+       I have worked with a range of technolgies building side projects as well as 
+       freelancing and building the projects for non-profit organizations. I'm specializing in a Front-End Development 
+       and I'm an avid Notes Taker. 
      </SectionText>
      <List>
        <ListItem>
-         <DiReact size="3rem" />
+         <CgWebsite size="3rem" />
          <ListContainer>
            <ListTitle>Front-End</ListTitle>
            <ListParagraph>
              Experience with <br />
-             React.js, Next.js, HTML, CSS, JavaScript
+             HTML5, CSS3 / SCSS, JavaScript + ES6
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+       <ListItem>
+         <DiReact size="3rem" />
+         <ListContainer>
+           <ListTitle>Front-End Frameworks</ListTitle>
+           <ListParagraph>
+             Experience with <br />
+             React.js, Angular, Next.js
            </ListParagraph>
          </ListContainer>
        </ListItem>
        <ListItem>
          <MdDesignServices size="3rem" />
          <ListContainer>
+           <ListTitle>UI Frameworks</ListTitle>
+           <ListParagraph>
+             Experience with <br />
+             Bootstrap, MaterialUI, SemanticUI, Ant-Design, Tailwind, Styled Components
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+       <ListItem>
+         <FaDatabase size="3rem" />
+         <ListContainer>
+           <ListTitle>Back-End</ListTitle>
+           <ListParagraph>
+             Basic experience with <br />
+             Node, Express, MongoDB, Rest API, GraphQL
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+       <ListItem>
+         <GrGraphQl size="3rem" />
+         <ListContainer>
+           <ListTitle>API Architecture</ListTitle>
+           <ListParagraph>
+             Experience with <br />
+             Rest API, GraphQL
+           </ListParagraph>
+         </ListContainer>
+       </ListItem>
+       <ListItem>
+         <FaSketch size="3rem" />
+         <ListContainer>
            <ListTitle>UI/UX</ListTitle>
            <ListParagraph>
-             Experience with <br />
+             Basic experience with <br />
              Sketch, Figma
-           </ListParagraph>
-         </ListContainer>
-       </ListItem>
-       <ListItem>
-         <BsPenFill size="3rem" />
-         <ListContainer>
-           <ListTitle>Technical Writing</ListTitle>
-           <ListParagraph>
-             An avid writer <br />
-             Technical Documentation
-           </ListParagraph>
-         </ListContainer>
-       </ListItem>
-       <ListItem>
-         <BsPenFill size="3rem" />
-         <ListContainer>
-           <ListTitle>General Knowledge</ListTitle>
-           <ListParagraph>
-             Experience with <br />
-             Node, Express, MongoDB, Rest API, GraphQL
            </ListParagraph>
          </ListContainer>
        </ListItem>
