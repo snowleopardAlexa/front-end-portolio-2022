253e6746eb294282f3fd660a65e95ff5774e32b5
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-20 09:00:18 -0500

    add accomplishments

--- src/components/Accomplishments/Accomplishments.jsx
+++ src/components/Accomplishments/Accomplishments.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+
+const data = [
+  { number: 3900, text: 'Total GitHub Commits'},
+  { number: 20, text: 'Projects I am proud of', },
+  { number: 201, text: 'Total Repositories'},
+  { number: 15, text: 'Technologies and Frameworks I built with'}
+];
 
 const Accomplishments = () => {
   return (
-    <div>Accomplishments</div>
-  )
+   <Section>
+     <SectionTitle>Personal Achievements</SectionTitle>
+     <Boxes>
+       {data.map((card, index) => (
+         <Box key={index}>
+           <BoxNum>{`${card.number}+`}</BoxNum>
+           <BoxText>{card.text}</BoxText>
+         </Box>
+       ))}
+     </Boxes>
+     <SectionDivider />
+   </Section>
+  );
 }
 
-export default Accomplishments
+export default Accomplishments;
