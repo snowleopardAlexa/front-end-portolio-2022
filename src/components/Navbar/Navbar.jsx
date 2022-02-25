429362120ddd139b5bc2d4cc53f628a4fbca4c7b
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-25 10:40:51 -0500

    add jsx function navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-//import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
-//import { FaTimes, FaBars } from 'react-icons/fa';
-import { Container, Nav, NavbarContainer, NavLogo, NavIcon, NavMenu, NavItem, NavLinks } from './NavbarStyles';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import { Container, Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarStyles';
 
 const Navbar = () =>  {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu  = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  useEffect(() => {
+    showButton()
+  }, [])
+
+  if (typeof window !== "undefined") {
+    window.addEventListener('resize', showButton);
+  }
 
   return (
   <Container>
@@ -14,23 +34,26 @@ const Navbar = () =>  {
         <NavIcon style={{float: 'left'}} />
          snowleopardAlexa
         </NavLogo>
-         <NavMenu style={{float: 'right'}}>
+        <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+         <NavMenu onClick={handleClick} click={click} style={{float: 'right'}}>
               <NavItem>
               </NavItem>
               <NavItem>
-                <NavLinks to="/about" href="#projects">Projects</NavLinks>
+                <NavLinks to="/about" href="#projects" onClick={closeMobileMenu}>Projects</NavLinks>
               </NavItem>
               <NavItem>
-                <NavLinks to="/services" href="#tech">Technologies</NavLinks>
+                <NavLinks to="/services" href="#tech" onClick={closeMobileMenu}>Technologies</NavLinks>
               </NavItem>
               <NavItem>
-                <NavLinks to="/products" href="#about">About</NavLinks>
+                <NavLinks to="/products" href="#about" onClick={closeMobileMenu}>About</NavLinks>
               </NavItem>
               <NavItem>
-                <NavLinks to="/products" href="#achievements">Achievements</NavLinks>
+                <NavLinks to="/products" href="#achievements" onClick={closeMobileMenu}>Achievements</NavLinks>
               </NavItem>
               <NavItem>
-                <NavLinks to="/products" href="#contact">Contact</NavLinks>
+                <NavLinks to="/products" href="#contact" onClick={closeMobileMenu}>Contact</NavLinks>
               </NavItem>
             </NavMenu>
       </NavbarContainer>
