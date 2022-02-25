3ff91a13a52ce87d65deec0dec7ce53a4972929f
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-25 09:39:54 -0500

    add function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { FaTimes, FaBars } from 'react-icons/fa';
 import { Container, Nav, NavLogo, NavbarContainer, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarStyles';
 
-const Navbar = () =>  (
+const Navbar = () =>  {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu  = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  useEffect(() => {
+    showButton()
+  }, [])
+
+  window.addEventListener('resize', showButton);
+
+  return (
   <Container>
      <Nav>
       <NavbarContainer>
