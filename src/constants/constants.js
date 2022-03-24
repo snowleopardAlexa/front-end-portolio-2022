b852c3fa9f88046364d4ed23d70f4faae4fe3435
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-24 17:29:49 -0400

    add linkedin clone

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,70 +1,80 @@
 export const alexaphoto = [
   {
     id: 0,
-    image: '/images/alexa.png'
-  }
+    image: "/images/alexa.png",
+  },
 ];
 
 export const projects = [
-    {
-      title: 'Instagram Clone',
-      description: "This is an Instagram Clone.",
-      image: '/images/instagram-clone.png',
-      tags: ['Next.js', 'Tailwind', 'Firebase', 'NextAuth'],
-      source: '',
-      visit: '',
-      id: 1,
-    },
-    {
-      title: 'Movies App',
-      description: "This is a Movies App. It fetches an API from TMDB and displays the movies.",
-      image: '/images/movies-app.png',
-      tags: ['React', 'Redux', 'SCSS'],
-      source: 'https://google.com',
-      visit: 'https://google.com',
-      id: 2,
-    },
-    {
-      title: 'Hulu Clone',
-      description: "This is a Hulu Clone.",
-      image: '/images/hulu-clone.png',
-      tags: ['Next.js', 'Tailwind'],
-      source: 'https://github.com/snowleopardAlexa/hulu-clone',
-      visit: 'https://hulu-clone-snowleopardalexa.vercel.app/',
-      id: 3,
-    },
-    {
-      title: 'Medium Clone',
-      description: "This is a Medium Clone. ",
-      image: '/images/medium-clone.png',
-      tags: ['React', 'TypeScript', 'SanityCMS', 'Tailwind'],
-      source: 'https://google.com',
-      visit: 'https://medium-clone-snowleopardalexa.vercel.app/',
-      id: 4,
-    },
-    {
-      title: 'Uber Clone',
-      description: "This is an Uber Clone.",
-      image: '/images/uber-clone.png',
-      tags: ['React Native', 'Tailwind', 'Redux Toolkit'],
-      source: '',
-      visit: '',
-      id: 5,
-    },
-    {
-      title: 'Beauty Salon',
-      description:"This is a Beauty Salon Website. It is a fully responsive.",
-      image: '/images/eden.png',
-      tags: ['React', 'Styled Components'],
-      source: 'https://eden-beauty-salon-website-snowleopardalexa.vercel.app/',
-      visit: '',
-      id: 6,
-    },
-  ];
-  
-  export const TimeLineData = [
-    { year: 2018, text: 'I started to learn programming.', },
-    { year: 2020, text: 'I began to work as a freelancer.', },
-    { year: 2021, text: 'I began to build personal projects.', },
-    { year: 2022, text: 'I am searching for a job at the company.', },
-  ];
+  {
+    title: "Linkedin Clone",
+    description: "This is an Instagram Clone.",
+    image: "/images/linkedin-clone.png",
+    tags: ["Next.js", "Tailwind", "Firebase", "NextAuth"],
+    source: "",
+    visit: "",
+    id: 1,
+  },
+  {
+    title: "Uber Clone",
+    description: "This is an Uber Clone.",
+    image: "/images/uber-clone.png",
+    tags: ["React Native", "Tailwind", "Redux Toolkit"],
+    source: "",
+    visit: "",
+    id: 2,
+  },
+  {
+    title: "Medium Clone",
+    description: "This is a Medium Clone. ",
+    image: "/images/medium-clone.png",
+    tags: ["React", "TypeScript", "SanityCMS", "Tailwind"],
+    source: "https://google.com",
+    visit: "https://medium-clone-snowleopardalexa.vercel.app/",
+    id: 3,
+  },
+  {
+    title: "Instagram Clone",
+    description: "This is an Instagram Clone.",
+    image: "/images/instagram-clone.png",
+    tags: ["Next.js", "Tailwind", "Firebase", "NextAuth"],
+    source: "",
+    visit: "",
+    id: 4,
+  },
+  {
+    title: "Hulu Clone",
+    description: "This is a Hulu Clone.",
+    image: "/images/hulu-clone.png",
+    tags: ["Next.js", "Tailwind"],
+    source: "https://github.com/snowleopardAlexa/hulu-clone",
+    visit: "https://hulu-clone-snowleopardalexa.vercel.app/",
+    id: 5,
+  },
+  {
+    title: "Movies App",
+    description:
+      "This is a Movies App. It fetches an API from TMDB and displays the movies.",
+    image: "/images/movies-app.png",
+    tags: ["React", "Redux", "SCSS"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 6,
+  },
+  {
+    title: "Beauty Salon",
+    description: "This is a Beauty Salon Website. It is a fully responsive.",
+    image: "/images/eden.png",
+    tags: ["React", "Styled Components"],
+    source: "https://eden-beauty-salon-website-snowleopardalexa.vercel.app/",
+    visit: "",
+    id: 7,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2018, text: "I started to learn programming." },
+  { year: 2020, text: "I began to work as a freelancer." },
+  { year: 2021, text: "I began to build personal projects." },
+  { year: 2022, text: "I am searching for a job at the company." },
+];
